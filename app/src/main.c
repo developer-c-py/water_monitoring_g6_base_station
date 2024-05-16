cc1fd1293f830357c04a19fc5c72995269bdc06a
developer-c-py <130222563+developer-c-py@users.noreply.github.com>
2024-05-16 15:26:23 +0200

    Update main.c
    application code updated

--- app/src/main.c
+++ app/src/main.c
@@ -1,31 +1,71 @@
-// /* Application Code */
+/**
+ * @file main.c
+ * @brief Application to interact with the water sensor driver
+ */
 
-#include <stdio.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
-#include <app_version.h>
+#include <string.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
-int main()
+#define SLEEP_TIME_MS 1000
+
+void main(void)
 {
+    const struct device *water_dev = DEVICE_DT_GET_ONE(zephyr_water);
+    struct sensor_value ph, temp, turb;
+    int ret;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-	const struct device *sensor;
-	sensor = device_get_binding("water");
-	if (!device_is_ready(sensor))
-	{
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+    if (!device_is_ready(water_dev)) {
+        LOG_ERR("Water sensor device not ready");
+        return;
+    }
 
-	while (1)
-	{
+    LOG_INF("Water sensor application started");
 
-		k_sleep(K_MSEC(1000));
-	}
+    while (1) {
+        /* Get pH value */
+        ret = sensor_sample_fetch(water_dev, WATER_CHAN_PH);
+        if (ret) {
+            LOG_ERR("Failed to fetch pH value (err %d)", ret);
+        } else {
+            ret = sensor_channel_get(water_dev, WATER_CHAN_PH, &ph);
+            if (ret) {
+                LOG_ERR("Failed to get pH value (err %d)", ret);
+            } else {
+                LOG_INF("pH: %d.%06d", ph.val1, ph.val2);
+            }
+        }
+
+        /* Get temperature value */
+        ret = sensor_sample_fetch(water_dev, WATER_CHAN_TEMP);
+        if (ret) {
+            LOG_ERR("Failed to fetch temperature value (err %d)", ret);
+        } else {
+            ret = sensor_channel_get(water_dev, WATER_CHAN_TEMP, &temp);
+            if (ret) {
+                LOG_ERR("Failed to get temperature value (err %d)", ret);
+            } else {
+                LOG_INF("Temperature: %d.%06d", temp.val1, temp.val2);
+            }
+        }
+
+        /* Get turbidity value */
+        ret = sensor_sample_fetch(water_dev, WATER_CHAN_TURB);
+        if (ret) {
+            LOG_ERR("Failed to fetch turbidity value (err %d)", ret);
+        } else {
+            ret = sensor_channel_get(water_dev, WATER_CHAN_TURB, &turb);
+            if (ret) {
+                LOG_ERR("Failed to get turbidity value (err %d)", ret);
+            } else {
+                LOG_INF("Turbidity: %d.%06d", turb.val1, turb.val2);
+            }
+        }
+
+        k_msleep(SLEEP_TIME_MS);
+    }
 }
